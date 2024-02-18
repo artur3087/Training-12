3e15b1793e66a355ba28f0fece8d53cf871dcdf9
Artur <artur3087@gmail.com>
2024-02-18 19:17:28 +0000

    first commit

---
+++ src/app/Posts/page.js
@@ -0,0 +1,20 @@
+// Posts - page with main posts //
+
+export default function Posts () { // START of a default function Posts //
+
+    return ( // START of returning in Posts //
+    
+      <div>  
+        <h2>
+            Main
+        </h2>
+        <h3>Posts
+
+        </h3>
+    </div>
+    
+    
+    ); // END of returning in Posts //
+
+
+};// END of a default function Posts //
---
+++ src/app/UserPosts/page.js
@@ -0,0 +1,11 @@
+// User's page //
+// This is where user can log in and create his own posts// 
+// I thought that his posts could become public and go into main page for views//  ?? 
+
+export default function userPosts () { // START of a default function userPosts //
+
+    return (
+        <h2> LOL </h2>
+    )
+
+}; // END of a default function userPosts //
